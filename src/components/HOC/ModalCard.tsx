0450c911d382de5765a3986709a5cd4efc523824
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-13 17:04:59 +0545

    refactor: Remove comments from HOC

--- src/components/HOC/ModalCard.tsx
+++ src/components/HOC/ModalCard.tsx
@@ -10,11 +10,9 @@ type ModalCardProps = {
 const ModalCard = ({ onCross, children }: ModalCardProps) => {
   return (
     <Card>
-      {/* delete button */}
       <div className="flex justify-end">
         <Button onclick={onCross} text="X" />
       </div>
-      {/* children */}
       <div>{children}</div>
     </Card>
   );
--- src/components/HOC/listCard.tsx
+++ src/components/HOC/listCard.tsx
@@ -10,14 +10,11 @@ type ModalCardProps = {
 const ListCard = ({ onCheck, onCross, children }: ModalCardProps) => {
   return (
     <Card>
-      {/* children */}
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           <Button onclick={onCheck} text="o" />
           <div>{children}</div>
         </div>
-
-        {/* delete button */}
 
         <Button onclick={onCross} text="X" />
       </div>
